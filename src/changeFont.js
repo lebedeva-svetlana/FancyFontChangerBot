a26fe004143f909ce4f8dee7597dd35e11264972
Svetlana Lebedeva <91262515+lebedeva-svetlana@users.noreply.github.com>
2023-07-17 23:52:23 +0300

    Add project files.

---
+++ src/changeFont.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+
+module.exports.changeFont = function (text, font, alphabet) {
+    let changedText = '';
+
+    for (let i = 0; i < text.length; ++i) {
+        if (!alphabet.includes(text[i].toLowerCase())) {
+            changedText += text[i];
+            continue;
+        }
+
+        let isLower = text[i] === text[i].toLowerCase();
+
+        for (let j = 0; j < alphabet.length; ++j) {
+            if (text[i].toLowerCase() !== alphabet[j]) {
+                continue;
+            }
+
+            if (isLower) {
+                changedText += font.lowercase[j];
+                continue;
+            }
+
+            if ('uppercase' in font) {
+                changedText += font.uppercase[j];
+            } else {
+                changedText += font.lowercase[j];
+            }
+        }
+    }
+
+    return Buffer.from(changedText, 'utf8').toString();
+}
---
+++ src/translit.js
@@ -0,0 +1,78 @@
+﻿'use strict';
+
+function isUpper(index, text) {
+    let indexIsUpper = false;
+    if (text[index] !== undefined) {
+        if (text[index] !== text[index].toLowerCase()) {
+            indexIsUpper = true;
+        }
+    }
+    return indexIsUpper;
+}
+
+module.exports.translit = function (text) {
+    let translitDict = {
+        'а': 'a',
+        'б': 'b',
+        'в': 'v',
+        'г': 'g',
+        'д': 'd',
+        'е': 'e',
+        'ё': 'e',
+        'ж': 'zh',
+        'з': 'z',
+        'и': 'i',
+        'й': 'y',
+        'к': 'k',
+        'л': 'l',
+        'м': 'm',
+        'н': 'n',
+        'о': 'o',
+        'п': 'p',
+        'р': 'r',
+        'с': 's',
+        'т': 't',
+        'у': 'u',
+        'ф': 'f',
+        'х': 'h',
+        'ц': 'c',
+        'ч': 'ch',
+        'ш': 'sh',
+        'щ': 'sch',
+        'ь': '',
+        'ы': 'y',
+        'ъ': '',
+        'э': 'e',
+        'ю': 'yu',
+        'я': 'ya'
+    };
+
+    let translit_text = '';
+    for (let i = 0; i < text.length; ++i) {
+        let dict_letter = translitDict[text[i].toLowerCase()];
+
+        if (dict_letter === undefined) {
+            translit_text += text[i];
+            continue;
+        }
+
+        if (text[i] === text[i].toLowerCase()) {
+            translit_text += dict_letter;
+            continue;
+        }
+
+        let nextIsUpper = isUpper(i + 1, text);
+        let previousIsUpper = isUpper(i - 1, text);
+
+        if (dict_letter.length == 1 || nextIsUpper || previousIsUpper) {
+            dict_letter = dict_letter.toUpperCase();
+        }
+        else {
+            dict_letter = dict_letter.charAt(0).toUpperCase() + dict_letter.slice(1);
+        }
+
+        translit_text += dict_letter;
+    }
+
+    return translit_text;
+}
