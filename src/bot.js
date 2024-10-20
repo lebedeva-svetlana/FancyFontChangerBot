12e611c35ddb5eb3c2e4afa297e7110659ba8f0c
lebedeva-svetlana <svetlana-swanson@yandex.ru>
2024-10-20 19:10:40 +0300

    Добавьте файлы проекта.

---
+++ src/bot.js
@@ -0,0 +1,57 @@
+﻿'use strict';
+
+const { Telegraf } = require('telegraf');
+const { message } = require('telegraf/filters');
+
+const { RUSSIAN, ENGLISH, FONTS_RUSSIAN, FONTS_ENGLISH, PANGRAM_RUSSIAN, PANGRAM_ENGLISH } = require('./text.js');
+const { changeFont } = require('./changeFont.js');
+const { translit } = require('./translit.js');
+const { getAllCommandText } = require("./getAllCommandText");
+
+let BOT_TOKEN = ''; // Insert your bot token
+
+const bot = new Telegraf(BOT_TOKEN);
+
+let font = FONTS_RUSSIAN[0];
+let alphabet = RUSSIAN;
+
+const regexRussian = new RegExp(`^/russian[1-${FONTS_RUSSIAN.length}]$`);
+const regexEnglish = new RegExp(`^/english[1-${FONTS_ENGLISH.length}]$`);
+
+bot.catch((err) => {
+    console.log(err);
+});
+
+bot.start(async (ctx) => {
+    let name = ctx.message.from.username || ctx.message.from.id;
+    await ctx.replyWithHTML(`Здравствуйте, ${name}. Отправьте текст для преобразования.`);
+});
+
+bot.command('help', async (ctx) => {
+    await ctx.reply(getAllCommandText());
+});
+
+bot.hears(regexRussian, async (ctx) => {
+    alphabet = RUSSIAN;
+    font = FONTS_RUSSIAN[ctx.message.text.slice(8, 9) - 1];
+    await ctx.reply(`Шрифт успешно изменён. Пример текста:\n\n${changeFont(PANGRAM_RUSSIAN, font, alphabet)}`);
+});
+
+bot.hears(regexEnglish, async (ctx) => {
+    alphabet = ENGLISH;
+    font = FONTS_ENGLISH[ctx.message.text.slice(8, 9) - 1];
+    await ctx.reply(`Шрифт успешно изменён. Пример текста:\n\n${changeFont(PANGRAM_ENGLISH, font, alphabet)}`);
+});
+
+bot.on(message('text'), async (ctx) => {
+    let message = ctx.message.text;
+    if (alphabet == ENGLISH) {
+        message = translit(ctx.message.text);
+    }
+    await ctx.replyWithHTML(`Нажмите на текст, чтобы скопировать его.\n\n<code>${changeFont(message, font, alphabet)}</code>`);
+});
+
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
---
+++ src/getAllCommandText.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+const { RUSSIAN, ENGLISH, FONTS_RUSSIAN, FONTS_ENGLISH, PANGRAM_RUSSIAN, PANGRAM_ENGLISH } = require('./text.js');
+const { changeFont } = require('./changeFont.js');
+
+module.exports.getAllCommandText = function () {
+    let message = 'Команды выбора шрифта:\n\n';
+    for (let i = 0; i < FONTS_RUSSIAN.length; ++i) {
+        message += `/russian${i + 1} — ${changeFont(PANGRAM_RUSSIAN, FONTS_RUSSIAN[i], RUSSIAN)}\n`;
+    }
+    message += '\n';
+    for (let i = 0; i < FONTS_ENGLISH.length; ++i) {
+        message += `/english${i + 1} — ${changeFont(PANGRAM_ENGLISH, FONTS_ENGLISH[i], ENGLISH)}\n`;
+    }
+    return message;
+}
